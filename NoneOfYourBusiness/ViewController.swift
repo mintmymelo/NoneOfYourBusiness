5dd5d89e5ff9c722a18838250e7aad54c2126295
Penpitcha Natisupalak <penpitcha@Penpitchas-MacBook-Pro.local>
2016-10-22 20:41:39 +0700

    Create new project

---
+++ NoneOfYourBusiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoneOfYourBusiness
+//
+//  Created by Penpitcha Natisupalak on 10/22/2559 BE.
+//  Copyright © 2559 Penpitcha Natisupalak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
